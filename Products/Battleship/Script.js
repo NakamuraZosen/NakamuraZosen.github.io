6d7a3849a84a06deae256748f7f488f0c4d83a63
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2021-09-29 10:13:50 +0900

    Delete Script.js

--- Products/Battleship/Script.js
+++
@@ -1,6 +0,0 @@
-function hamburger() {
-    document.getElementById('header__nav').classList.toggle('header__nav-open');
-  }
-  document.getElementById('header__nav-button').addEventListener('click' , function() {
-    hamburger();
-  } , false);
