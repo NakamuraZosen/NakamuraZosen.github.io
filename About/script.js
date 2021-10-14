d99359ee8ce301874df50dce8ed51ba11707035c
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2021-10-14 19:32:34 +0900

    Update script.js

--- About/script.js
+++ About/script.js
@@ -1,6 +1,18 @@
+document.getElementById('header__nav-button').addEventListener('click' , function() {
+  hamburger();
+} , false);
 function hamburger() {
-    document.getElementById('header__nav').classList.toggle('header__nav-open');
+  document.getElementById('header__nav').classList.toggle('header__nav-open');
+}
+
+window.onload = function() {
+  var CookieData = document.cookie + ';';
+  var StartPositionOfVisited = CookieData.indexOf('visited=',0);
+
+  if (StartPositionOfVisited == -1) {
+    alert('初めまして');
+    document.cookie = 'visited=true';
+  } else {
   }
-  document.getElementById('header__nav-button').addEventListener('click' , function() {
-    hamburger();
-  } , false);
+  console.log(document.cookie);
+}
