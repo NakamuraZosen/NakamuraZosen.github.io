ab4a1dda102c80419330173045e528408f110b04
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2021-10-14 19:09:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+document.getElementById('header__nav-button').addEventListener('click' , function() {
+  hamburger();
+} , false);
 function hamburger() {
-    document.getElementById('header__nav').classList.toggle('header__nav-open');
-  }
-  document.getElementById('header__nav-button').addEventListener('click' , function() {
-    hamburger();
-  } , false);
-
-function writecookie() {
-  document.cookie = 'visited=true';
-  console.log(document.cookie);
+  document.getElementById('header__nav').classList.toggle('header__nav-open');
 }
+
+var CookieData = document.cookie + ';';
+var StartPositionOfVisited = CookieData.indexOf('visited=',0);
+
+if (StartPositionOfVisited == -1) {
+  alert('初めまして');
+  document.cookie = 'visited=true';
+} else {
+}
+console.log(document.cookie);
