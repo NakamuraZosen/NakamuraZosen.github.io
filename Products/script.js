b4b3c3d00087bda9077ab401c719bc3cbed9215b
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2021-10-14 20:21:07 +0900

    Update script.js

--- Products/script.js
+++ Products/script.js
@@ -1,6 +1,20 @@
+document.getElementById('header__nav-button').addEventListener('click' , function() {
+  hamburger();
+} , false);
 function hamburger() {
-    document.getElementById('header__nav').classList.toggle('header__nav-open');
+  document.getElementById('header__nav').classList.toggle('header__nav-open');
+}
+
+window.onload = function() {
+  var CookieData = document.cookie + ';';
+  var StartPositionOfVisited = CookieData.indexOf('visited=',0);
+
+  if (StartPositionOfVisited == -1) {
+    alert('なかむらのホームページへようこそ。もしよければブックマークしてください。検索しても出てきませんから( ﾉД`)ｼｸｼｸ…');
+    var expire = new Date();
+    expire.setTime( expire.getTime() + 1000 * 3600 * 24 * 365 );
+    document.cookie = 'visited=true; path=/; expires='+ expire;
+  } else {
   }
-  document.getElementById('header__nav-button').addEventListener('click' , function() {
-    hamburger();
-  } , false);
+  console.log(document.cookie);
+}
