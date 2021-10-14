2088dd8b5a325a5b71716572e7ef63758402b197
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2021-10-14 18:00:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function hamburger() {
   document.getElementById('header__nav-button').addEventListener('click' , function() {
     hamburger();
   } , false);
+
+function writecookie() {
+  document.cookie = 'visited=true';
+}
+
+function readcookie() {
+  if (document.cookie == 'visited=true') {
+    
+  } else {
+    alert('初めて');
+  }
+}
