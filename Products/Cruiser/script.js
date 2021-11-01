20b7ae3f97227f88bafcded947e63c41b7263d19
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2021-11-01 17:58:08 +0900

    Update script.js

--- Products/Cruiser/script.js
+++ Products/Cruiser/script.js
@@ -8,7 +8,7 @@ openButton.addEventListener('click', openModal);
 function openModal() {
   modalBackground.style.display = 'flex';
   if (loaded == false) {
-    closeButton.insertAdjacentHTML('beforebegin','<iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/eWcYph5I_84" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    closeButton.insertAdjacentHTML('beforebegin','<iframe width="560" height="315" src="https://www.youtube.com/embed/5xruhgVadps" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     loaded = true;
   };
 };
