5d4917498d5df43d8c26e75b21b9d42c9afcc5c8
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2021-11-01 17:32:45 +0900

    Create script.js

---
+++ Products/Battleship/script.js
@@ -0,0 +1,20 @@
+//modal controller of youtube iframe
+var openButton = document.getElementById('relatedMovie__button');
+var closeButton = document.getElementById('iframeModal__closeButton');
+var modalBackground = document.getElementById('iframeModal__background');
+var loaded = false;
+
+openButton.addEventListener('click', openModal);
+function openModal() {
+  modalBackground.style.display = 'flex';
+  if (loaded == false) {
+    closeButton.insertAdjacentHTML('beforebegin','<iframe width="560" height="315" src="https://www.youtube.com/embed/m7SgsqS9Woo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    loaded = true;
+  };
+};
+
+closeButton.addEventListener('click', closeModal);
+modalBackground.addEventListener('click', closeModal);
+function closeModal() {
+  modalBackground.style.display = 'none';
+};
