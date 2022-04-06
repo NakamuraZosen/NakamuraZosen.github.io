11ff4cfcd4a10a05355b84429c7da320f1a98df1
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2022-04-06 17:16:23 +0900

    Create script.js

---
+++ Statistics/createrList/script.js
@@ -0,0 +1,59 @@
+const output_csv_element = document.querySelector('.output_csv');
+
+// CSVの読み込み
+function import_csv(csv_path) {
+  fetch(csv_path)
+    .then((res) => {
+      if (!res.ok) {
+        console.log('正常にリクエストを処理できませんでした。');
+      }
+      return res.text();
+    })
+    .then((csv_data) => {
+      convert_array(csv_data);
+    })
+    .catch((error) => {
+      console.log('エラーが発生しました。');
+    })
+}
+
+// テキストデータを配列に変換
+function convert_array(csv_data) {
+  let data_array = [];
+  const data_string = csv_data.split('\n');
+  for (let i = 0; i < data_string.length-1; i++) {
+    data_array[i] = data_string[i].split(',');
+  }
+  //cancel auto generated break on gituhb; add lengh-1
+  draw_table(data_array);
+}
+
+// テーブル描画
+function draw_table(data_array) {
+  data_array.forEach((elements,index) => {
+    const tr = document.createElement('tr');
+    tr.id = 'no' + (index + 1);
+    output_csv_element.appendChild(tr);
+    elements.forEach(element => {
+      let td = document.createElement('td');
+      if (element.startsWith('https://') === true) {
+        let anchor = document.createElement('a');
+        anchor.href = element
+        anchor.target = '_blank'
+        anchor.innerText = element
+        td.appendChild(anchor);
+      } else {
+        td.innerText = element
+      }
+      tr.appendChild(td);
+    });
+  });
+  //tablesorter
+  $(function() {
+    $(".sort-table").tablesorter();
+  });
+}
+
+import_csv('https://NakamuraZosen.github.io/Statistics/createrList/createrList.csv');
+
+//reference: freelance321.com/javascript/load-csv/
