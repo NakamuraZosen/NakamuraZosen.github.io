c10fcbfac467eba3106e24012bb8ec8fd62407e9
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2022-05-20 18:16:58 +0900

    Update script.js

--- Events/NKM2022/script.js
+++ Events/NKM2022/script.js
@@ -8,7 +8,7 @@ openButton.addEventListener('click', openModal);
 function openModal() {
   modalBackground.style.display = 'flex';
   if (loaded == false) {
-    closeButton.insertAdjacentHTML('beforebegin','<iframe width="560" height="315" src="https://www.youtube.com/embed/m7SgsqS9Woo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    closeButton.insertAdjacentHTML('beforebegin','<iframe width="560" height="315" src="https://www.youtube.com/embed/woVdxUrqNvQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     loaded = true;
   };
 };
