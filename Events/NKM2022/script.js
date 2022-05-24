4e8920cba08d453e6c26db26a1bda004ff3eab05
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2022-05-24 19:43:28 +0900

    Update script.js

--- Events/NKM2022/script.js
+++ Events/NKM2022/script.js
@@ -18,3 +18,16 @@ modalBackground.addEventListener('click', closeModal);
 function closeModal() {
   modalBackground.style.display = 'none';
 };
+
+//date count down
+var now = new Date();
+var point = new Date("2022/06/19");
+var countdown = Math.ceil((point.getTime() - now.getTime()) / (1000 * 60 * 60 * 24));
+console.log(countdown);
+if (countdown > -1) {
+  var deadline = '応募締め切りまであと' + countdown + '日です。';
+  document.getElementById('deadline').insertAdjacentHTML('afterbegin',deadline);
+} else {
+  var deadline = '応募は' + (-1 * countdown) + '日前に締め切られました。';
+  document.getElementById('deadline').insertAdjacentHTML('afterbegin',deadline);
+};
