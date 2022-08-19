65cacecc030349b64a1fbd31eaf633db8c4e4199
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2022-08-19 20:58:30 +0900

    Update shareScript.js

--- shareScript.js
+++ shareScript.js
@@ -34,10 +34,11 @@ header.insertAdjacentHTML('afterbegin', `
 
 <nav id="header__nav">
   <ul class="header__list">
-    <li class="header__item"><a href="https://NakamuraZosen.github.io/Products/index.html" class="header__item-link"><span>作品</span></a></li>
-    <li class="header__item"><a href="https://NakamuraZosen.github.io/Events/index.html" class="header__item-link"><span>イベント情報</span></a></li>
-    <li class="header__item"><a href="https://NakamuraZosen.github.io/News/index.html" class="header__item-link"><span>ニュース</span></a></li>
-    <li class="header__item"><a href="https://NakamuraZosen.github.io/About/index.html" class="header__item-link"><span>中村とは</span></a></li>
+    <li class="header__item"><a href="https://NakamuraZosen.github.io/Products/index.html" class="header__item-link"><span>作品</span><small>products</small></a></li>
+    <li class="header__item"><a href="https://NakamuraZosen.github.io/Tech/index.html" class="header__item-link"><span>イベント情報</span><small>events</small></a></li>
+    <li class="header__item"><a href="https://NakamuraZosen.github.io/Statistics/createrList/index.html" class="header__item-link"><span>中村センサス</span><small>census</small></a></li>
+    <li class="header__item"><a href="https://NakamuraZosen.github.io/News/index.html" class="header__item-link"><span>ニュース</span><small>news</small></a></li>
+    <li class="header__item"><a href="https://NakamuraZosen.github.io/About/index.html" class="header__item-link"><span>中村とは</span><small>about</small></a></li>
   </ul>
 </nav>
 </div>
