8f90f00a44ee5eb529cb6e042691394fa1232c1d
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2023-03-21 16:10:57 +0900

    Update script.js

--- Statistics/createrList/script.js
+++ Statistics/createrList/script.js
@@ -1,89 +1,166 @@
 const output_csv_element = document.querySelector('.output_csv');
+const thead = document.createElement('thead');
+const tbody = document.createElement('tbody');
+output_csv_element.appendChild(thead);
+output_csv_element.appendChild(tbody);
+let data_array = [];
 
-// CSVの読み込み
-function import_csv(csv_path) {
-  fetch(csv_path)
+// jsonの読み込み
+function import_json(json_path) {
+  fetch(json_path)
     .then((res) => {
       if (!res.ok) {
         console.log('正常にリクエストを処理できませんでした。');
       }
-      return res.text();
+      return res.json();
     })
-    .then((csv_data) => {
-      convert_array(csv_data);
-    })
-    .catch((error) => {
-      console.log('エラーが発生しました。');
+    .then((data) => {
+      memorise_json(data);
     })
 }
+// jsonをグローバル変数に代入
+function memorise_json(data) {
+  console.log(data);
+  data_array = data;
+  draw_table();
+}
 
-// テキストデータを配列に変換
-function convert_array(csv_data) {
-  let data_array = [];
-  const data_string = csv_data.split('\n');
-  for (let i = 0; i < data_string.length-1; i++) {
-    data_array[i] = data_string[i].split(',');
+//　検索とフィルタを読み取り
+const refresh_button = document.querySelector('#refresh_table');
+refresh_button.addEventListener('click', refresh_table);
+// テーブルを更新
+function refresh_table() {
+  // チェックボックスを読み取り
+  const chk1 = document.form1.tag;
+  let selectedTags = [];
+  for (let i = 0; i < chk1.length; i++) {
+    if (chk1[i].checked) {//(chk1[i].checked === true)と同じ
+      selectedTags.push(chk1[i].value);
+    }
   }
-  //cancel auto generated break on gituhb; add lengh-1
-  draw_table(data_array);
-}
+  console.log('filter refreshed: ');
+  console.log(selectedTags);
 
-// テーブル描画
-function draw_table(data_array) {
-  //append thead and tbody
-  const thead = document.createElement('thead');
-  const tbody = document.createElement('tbody');
-  output_csv_element.appendChild(thead);
-  output_csv_element.appendChild(tbody);
+  //現在のテーブルを削除
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
 
-  //create thead  
-  const thead_tr = document.createElement('tr');
-  thead.appendChild(thead_tr);
-  data_array[0].forEach(element => {
-    let th = document.createElement('th');
-    th.innerText = element
-    thead_tr.appendChild(th);
-  });
-
-  //create tbody
-  data_array.shift();
-
-  data_array.forEach((elements,index) => {
-    let tr = document.createElement('tr');
-    tr.id = 'no' + (index + 1);
-    tbody.appendChild(tr);
-
-    elements.forEach(element => {
-      let td = document.createElement('td');
-      if (element.startsWith('https://') === true) {
+  // テーブル本体を作成
+  for (var i = 0; i < data_array.length; i++) {
+    // チェックされている場合、タグが選択されてい無ければ飛ばす。
+    if (selectedTags.length !== 0) {
+      const isIncludes = (arr, target) => arr.some(el => target.includes(el));
+      if (isIncludes(data_array[i]['tags'], selectedTags) === false) {
+        console.log('skipped');
+        continue;
+      }
+    }
+    // tr要素を生成
+    var tr = document.createElement('tr');
+    tr.id = 'no' + (i + 1);
+    // th・td部分のループ
+    for (var key in data_array[0]) {
+      // td要素を生成
+      var td = document.createElement('td');
+      // td要素内にテキストを追加
+      // idならリンク化
+      if (key === "リンク") {
         let anchor = document.createElement('a');
-        anchor.href = element
+        anchor.href = data_array[i][key]
         anchor.target = '_blank'
-        anchor.innerText = element
+        anchor.innerText = key
         td.appendChild(anchor);
       } else {
-        td.innerText = element
-      };
-        tr.appendChild(td);
-    });
-  });
+        //　リンクでなければ追加
+        td.textContent = data_array[i][key];
+      }
+      // td要素をtr要素の子要素に追加
+      tr.appendChild(td);
+    }
+    // tr要素をtable要素の子要素に追加
+    tbody.appendChild(tr);
+  }
+  document.getElementById('hit_channels').innerText = 'ヒットしたチャンネル数：' + tbody.children.length;
+  //tablesorter更新
+  $(".sort-table").trigger("update");
+}
+
+// テーブルを描画
+function draw_table() {
+
+  // ヘッダーを作成
+  const thead_tr = document.createElement('tr');
+  for (var key in data_array[0]) {
+    console.log(key + ': ' + data_array[0]);
+    // th要素を生成
+    var th = document.createElement('th');
+    // th要素内にテキストを追加
+    th.textContent = key;
+    // th要素をtr要素の子要素に追加
+    thead_tr.appendChild(th);
+  }
+  // tr要素をtable要素の子要素に追加
+  thead.appendChild(thead_tr);
+
+  // テーブル本体を作成
+  for (var i = 0; i < data_array.length; i++) {
+    // tr要素を生成
+    var tr = document.createElement('tr');
+    tr.id = 'no' + (i + 1);
+    // th・td部分のループ
+    for (var key in data_array[0]) {
+      // td要素を生成
+      var td = document.createElement('td');
+      // td要素内にテキストを追加
+      // idならリンク化
+      if (key === "リンク") {
+        let anchor = document.createElement('a');
+        anchor.href = data_array[i][key]
+        anchor.target = '_blank'
+        anchor.innerText = key
+        td.appendChild(anchor);
+      } else {
+        //　リンクでなければ追加
+        td.textContent = data_array[i][key];
+      }
+      // td要素をtr要素の子要素に追加
+      tr.appendChild(td);
+    }
+    // tr要素をtable要素の子要素に追加
+    tbody.appendChild(tr);
+  }
+
   //channel quantity
   document.getElementById('channels').innerText = '調査済みチャンネル数：' + data_array.length;
   //tablesorter
-  $(function() {
+  $(function () {
     $(".sort-table").tablesorter();
   });
 }
 
+
+
+
 //select research date form url query parameter
 const params = new URLSearchParams(window.location.search);
 const target = document.getElementById('researchDate');
+
 if (params.has("date") == true) {
-  target.innerHTML = "調査日時：" + params.get("date");
-  import_csv('https://NakamuraZosen.github.io/Statistics/createrList/createrList_' + params.get("date") + '.csv');
+  target.innerText = "調査日時：" + params.get("date");
+  let json_path = 'https://NakamuraZosen.github.io/Statistics/createrList/createrList_' + params.get("date") + '.json';
+  import_json(json_path);  
 } else {
-  target.innerHTML = "調査日時が選択されていません。";
+  target.innerText = "調査日時が選択されていません。";
+}
+// 古い調査ではタグ検索を隠す
+console.log(Number(params.get("date")));
+if (Number(params.get("date")) < 20230000 ) {
+  document.getElementById('filter__container').style.display = "none";
 }
 
 //reference: freelance321.com/javascript/load-csv/
 //reference: www.ipentec.com/document/javascript-get-url-parameter
+//reference: qiita.com/kunihiro9216/items/15339509604d66fdb961
+//reference: itsakura.com/js-checkbox
+//reference(array includes): qiita.com/mtoutside/items/e34e06c674a8e6623ba4
