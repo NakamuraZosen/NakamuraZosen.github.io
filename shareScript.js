2401047250d8620f34cffc2f89a7ff61ebafde0d
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2024-04-22 20:49:40 +0900

    Update shareScript.js

--- shareScript.js
+++ shareScript.js
@@ -13,6 +13,7 @@
 };
 */
 
+
 //include header
 let header = document.getElementById('header');
 header.insertAdjacentHTML('afterbegin', `
@@ -26,7 +27,7 @@ header.insertAdjacentHTML('afterbegin', `
       NNZ SHIPBUILDING
     </span>
   </a>
-  
+
   <button type="button" id="header__nav-button">
     <span class="header__nav-button-line"></span>
   </button>
@@ -46,16 +47,35 @@ header.insertAdjacentHTML('afterbegin', `
 //include footer
 let footer = document.getElementById('footer');
 footer.insertAdjacentHTML('afterbegin', `
-<div class="footer__sns">
-  <p class="footer__sns-index">SNS</p>
-  <ul class="footer__sns-list">
-    <li class="footer__item"><a href="https://www.youtube.com/channel/UCzB8_PC3Siup1Zai8EfJC2g" target="_blank"
-      rel="noopener noreferrer" class="footer__item-link">公式YouTube<span class="material-symbols-outlined">open_in_new</span></a></li>
-    <li class="footer__item"><a href="https://twitter.com/NHI_nakamura" target="_blank" rel="noopener noreferrer"
-      class="footer__item-link">公式Twitter<span class="material-symbols-outlined">open_in_new</span></a></li>
-  </ul>
+<div class="footer__wrapper">
+  <div class="footer__content">
+    <span class="footer__content-index">SNS</span>
+    <ul class="footer__content-list">
+      <li class="footer__item"><a href="https://www.youtube.com/channel/UCzB8_PC3Siup1Zai8EfJC2g" target="_blank" rel="noopener noreferrer" class="footer__item-link">公式YouTube<span class="material-symbols-outlined">open_in_new</span></a></li>
+      <li class="footer__item"><a href="https://twitter.com/NHI_nakamura" target="_blank" rel="noopener noreferrer" class="footer__item-link">公式Twitter<span class="material-symbols-outlined">open_in_new</span></a></li>
+    </ul>
+  </div>
+  <div class="footer__content">
+    <span class="footer__content-index">HPの不具合等</span>
+    <ul class="footer__content-list">
+      <li class="footer__item"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdh8NzM_RGn50L5WsNhh13nltpO50aodwJcDDqJiKV-cVgtsA/viewform?usp=sf_link">Google forms<span class="material-symbols-outlined">open_in_new</span></a></li>
+    </ul>
+  </div>
+  <!-- 切り替えスイッチ -->
+  <div class="footer__content">
+    <span class="footer__content-index">暗い背景</span>
+    <input
+      type="checkbox"
+      name="onoffswitch"
+      class="onoffswitch-checkbox"
+      id="myonoffswitch"
+      tabindex="0"
+    />
+    <label class="onoffswitch-label" for="myonoffswitch">
+    </label>
+  </div>
 </div>
-<p class="footer__copyright">©2024 NNZ Shipbuilding, All rights reserved.</p>
+<span class="footer__copyright">©2024 NNZ Shipbuilding, All rights reserved.</span>
 `);
 
 //humburger menu controller on phone
@@ -66,6 +86,50 @@ document.getElementById('header__nav-button').addEventListener('click', function
 function hamburger() {
   document.getElementById('header__nav').classList.toggle('header__nav-open');
 };
+
+//toggle darkmode reference: https://zenn.dev/chabatake_i/articles/darkmode_introductions
+// OSの設定がダークモード
+const osDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+// イベントリスナー
+const listener = (event) => {
+  if (event.matches) {
+    darkModeOn();
+  } else {
+    darkModeOff();
+  }
+};
+
+// リスナー登録
+//osDark.addEventListener("change", listener);
+
+
+// スイッチのinput要素（checkbox）
+const modeSwitch = document.getElementById("myonoffswitch");
+
+// スイッチの操作に応じて切り替え処理
+modeSwitch.addEventListener("change", () => {
+  if (modeSwitch.checked) {
+    darkModeOn();
+    } else {
+    darkModeOff();
+  }
+});
+
+// ダークモードがオンの時に実行する処理
+function darkModeOn() {
+  document.documentElement.classList.add("darkmode"); // ルート要素<html>にクラスを追加
+  modeSwitch.checked = true;
+};
+// ダークモードがオフの時に実行する処理
+function darkModeOff() {
+  document.documentElement.classList.remove("darkmode"); // クラスの削除
+  modeSwitch.checked = false;
+};
+
+// 初期化処理
+//listener(osDark);
+
 /*
 var openButton = document.getElementById('header__nav-button');
 var closeButton = document.getElementById('header__nav-button');
