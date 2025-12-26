6efffc9846a6d3bbdb8917b8f0a29c71d6ebdc41
NakamuraZosen <90998073+NakamuraZosen@users.noreply.github.com>
2025-12-26 11:46:03 +0900

    Update shareScript.js

--- shareScript.js
+++ shareScript.js
@@ -99,7 +99,7 @@ footer.insertAdjacentHTML('afterbegin', `
         </div>
         <div class="footer__content">
           <span class="footer__content-index">問い合わせ</span>
-          <a href="#">
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSdh8NzM_RGn50L5WsNhh13nltpO50aodwJcDDqJiKV-cVgtsA/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
             サイトの不具合等報告フォーム
             <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 -960 960 960">
               <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"></path>
